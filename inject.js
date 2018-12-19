9afa93665a679d0ae5d72b0b450128ebe0775d1b
Eduardo Telaya <luis.eduardo.telaya@gmail.com>
2018-12-18 23:58:14 -0500

    Initial version - launch

---
+++ inject.js
@@ -0,0 +1,21 @@
+// this is the code which will be injected into a given page...
+
+(function() {
+
+  window.scrollTo(0,document.body.scrollHeight);
+  window.scrollTo(0,document.body.scrollHeight);
+  window.scrollTo(0,document.body.scrollHeight);
+  window.scrollTo(0,document.body.scrollHeight);
+  window.scrollTo(0,document.body.scrollHeight);
+  window.scrollTo(0,document.body.scrollHeight);
+  window.scrollTo(0,document.body.scrollHeight);
+  window.scrollTo(0,document.body.scrollHeight);
+  window.scrollTo(0,document.body.scrollHeight);
+  window.scrollTo(0,document.body.scrollHeight);
+  window.scrollTo(0,document.body.scrollHeight);
+  window.scrollTo(0,document.body.scrollHeight);
+  var divs = document.querySelectorAll('.button-secondary-small');
+  for (i = 0; i < divs.length; ++i) {
+    divs[i].click();
+  };
+})();
